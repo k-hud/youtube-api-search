6ff68116471236cf1c93016f20f829e34092923c
Kyle Hudson <14058890+kylemhudson@users.noreply.github.com>
2018-11-29 23:14:40 -0500

    Calling it. Push on API GET call and format params tomm.

--- script.js
+++ script.js
@@ -1,72 +1,72 @@
-
 function eventListeners(event) {
-    $('#js-park-input').on('input', function () {
-      if ($(this).val().length) {
-        $('#js-search-input').prop('disabled', true);
-        $('#js-search-input').css('background-color', '#d1d1d1');
-      } else {
-        $('#js-search-input').prop('disabled', false);
-      }
-    });
+  $("#js-park-input").on("input", function() {
+    if ($(this).val().length) {
+      $("#js-search-input").prop("disabled", true);
+      $("#js-search-input").css("background-color", "#d1d1d1");
+    } else {
+      $("#js-search-input").prop("disabled", false);
+    }
+  });
 
-    $('#js-search-input').on('input', function () {
-      if ($(this).val().length) {
-        $('#js-park-input').prop('disabled', true);
-        $('#js-park-input').css('background-color', '#d1d1d1');
-      } else {
-        $('#js-park-input').prop('disabled', false);
-      }
-    });
+  $("#js-search-input").on("input", function() {
+    if ($(this).val().length) {
+      $("#js-park-input").prop("disabled", true);
+      $("#js-park-input").css("background-color", "#d1d1d1");
+    } else {
+      $("#js-park-input").prop("disabled", false);
+    }
+  });
 
-    $('#js-search-form').submit(event => {
+  $("#js-search-form").submit(event => {
     event.preventDefault();
 
-    let parkInput = $('#js-park-input').val();
-    let searchInput = $('#js-search-input').val();
-    let stateInput = $('#js-state-input').val();
-    let resultNumInput = $('#js-result-num-input').val();
+    let parkInput = $("#js-park-input").val();
+    let searchInput = $("#js-search-input").val();
+    let stateInput = $("#js-state-input").val();
+    let resultNumInput = $("#js-result-num-input").val();
 
     console.log(parkInput, searchInput, stateInput, resultNumInput);
     console.log(parkInput.length, searchInput.length);
 
     if (parkInput.length == 0) {
-
       apiPull(searchInput, stateInput, resultNumInput);
-
     } else if (searchInput.length == 0) {
-
       apiPull(parkInput, stateInput, resultNumInput);
-
     } else {
-
       console.log("Something broke.");
-
     }
   });
 }
 
 function apiPull(parkOrSearch, stateInput, resultNumInput) {
-  console.log('Made it!');
-  // console.log(parkOrSearch, stateInput, resultNumInput);
-  // const url = '';
-  //
-  // const queryParams = {
-  //   key: '579lGWHjB1vqN5cUJw6TDoY6t1gUP7h6LMswisjj';
-  // };
-  //
-  // fetch(url)
-  // .then()
-  // .then()
-  // .catch();
+
+  console.log(parkOrSearch, stateInput, resultNumInput);
+  const url = 'https://api.nps.gov/api/v1/parks';
+
+  const queryParams = {
+    key: '579lGWHjB1vqN5cUJw6TDoY6t1gUP7h6LMswisjj',
+    q: parkOrSearch,
+    parkCode: parkOrSearch,
+    stateCode: stateInput,
+    limit: resultNumInput,
+  };
+
+  formatParams(url, queryParams);
+
+  fetch(url)
+  .then(response => response.json())
+  .then(responseJson => displayResults(responseJson))
+  .catch(error => console.log(error.message));
 }
 
-function formatQuery() {
-
-}
-
-function displayResults() {
-  $('#js-display-results').empty();
-
-}
+// function formatParams() {
+//   const query = {
+//
+//   }
+// }
+//
+// function displayResults() {
+//   $("#js-display-results").empty();
+// }
 
 $(eventListeners);
